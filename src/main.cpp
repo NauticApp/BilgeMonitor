01a48ff23126ad6fdf33339ce942820d2bc836b5
dcoldeira <dcoldeira@gmail.com>
2023-10-13 09:17:32 +0100

    adding code and README

---
+++ src/main.cpp
@@ -0,0 +1,79 @@
+#include <Wire.h>
+#include <WiFi.h>
+#include <WebServer.h>
+#include <ArduinoJson.h>
+
+const int trigPin = 4;
+const int echoPin = 5;
+
+const char* ssid = "VM9840342";
+const char* password = "gmjy3VwBvwrk";
+
+const int esp32Port = 80;
+
+WebServer server(esp32Port);
+
+float readDistance() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  unsigned long duration = pulseIn(echoPin, HIGH);
+  float distanceCentimeters = (duration / 2.0) * 0.0343; // Convert to centimeters
+
+  return distanceCentimeters;
+}
+
+void handleRoot() {
+  String html = "<html><body>";
+  html += "<h1 style='text-align:center;'>V-03 BilgeMonitoring (ESP32 WROOM 32D with the HC-SRD4 Sensor)</h1>";
+  html += "<div style='text-align:center;'>";
+  html += "<h2>Distance</h2>";
+  html += "<p id='distance' style='font-size:48px;'></p>";
+  html += "</div>";
+  html += "<script>";
+  html += "setInterval(updateSensorReadings, 2000);";
+  html += "function updateSensorReadings() {";
+  html += "  fetch('/sensors')";
+  html += "    .then(response => response.json())";
+  html += "    .then(data => {";
+  html += "      document.getElementById('distance').textContent = data.Distance.toFixed(1) + ' cm';";
+  html += "    });";
+  html += "}";
+  html += "</script>";
+  html += "</body></html>";
+
+  server.send(200, "text/html", html);
+}
+
+void handleSensorData() {
+  String data = "{\"Distance\": " + String(readDistance(), 1) + "}";
+  server.send(200, "application/json", data);
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+
+  Serial.print("192.168.0.61");
+  Serial.println(WiFi.localIP());
+
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+  server.on("/", HTTP_GET, handleRoot);
+  server.on("/sensors", HTTP_GET, handleSensorData);
+  server.begin();
+}
+
+void loop() {
+  server.handleClient();
+  delay(1000);
+}
