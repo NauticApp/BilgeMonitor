573b536f4fc9f43c75096d75b33cc1c8d9de4e80
dcoldeira <dcoldeira@gmail.com>
2023-10-13 09:26:43 +0100

    Adding code + README

---
+++ .pio/build/esp32dev/FrameworkArduino/FirmwareMSC.cpp.d
@@ -0,0 +1,5 @@
+.pio/build/esp32dev/FrameworkArduino/FirmwareMSC.cpp.o: \
+ /home/dcoldeira/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.cpp \
+ /home/dcoldeira/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.h \
+ /home/dcoldeira/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
+ /home/dcoldeira/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
---
+++ .pio/build/esp32dev/lib58a/WebServer/detail/mimetable.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/esp32dev/lib58a/WebServer/detail/mimetable.cpp.o: \
+ /home/dcoldeira/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.cpp \
+ /home/dcoldeira/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.h \
+ /home/dcoldeira/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h
