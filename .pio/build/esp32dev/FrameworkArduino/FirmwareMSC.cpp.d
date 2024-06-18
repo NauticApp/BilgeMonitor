4b9041cd4a966bef4270b190df6c134c39f770a9
dcoldeira <dcoldeira@gmail.com>
2024-06-18 19:03:12 +0100

    git clone address fixed

--- .pio/build/esp32dev/FrameworkArduino/FirmwareMSC.cpp.d
+++
@@ -1,5 +0,0 @@
-.pio/build/esp32dev/FrameworkArduino/FirmwareMSC.cpp.o: \
- /home/dcoldeira/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.cpp \
- /home/dcoldeira/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.h \
- /home/dcoldeira/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
- /home/dcoldeira/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
--- .pio/build/esp32dev/lib58a/WebServer/detail/mimetable.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32dev/lib58a/WebServer/detail/mimetable.cpp.o: \
- /home/dcoldeira/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.cpp \
- /home/dcoldeira/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.h \
- /home/dcoldeira/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h
